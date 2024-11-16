7e50397184e2ae15432a8e252048c057d7b12ab8
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2024-11-16 20:09:29 +0000

    adicinei serializers

--- project/app/views/api.py
+++ project/app/views/api.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Participante
-from .serializers import ParticipanteSerializer
+from app.models import Participante #modifiquei
+from app.serializers import ParticipanteSerializer #modifiquei
 from rest_framework.permissions import AllowAny
 
 
